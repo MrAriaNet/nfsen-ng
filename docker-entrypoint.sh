68ee48e12deb6ca1dd4de0041971806745b8f518
Aria <47712536+MrAriaNet@users.noreply.github.com>
2025-09-29 11:35:06 +0330

    Update docker-entrypoint.sh
    directory update to nfsen-ng

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,18 @@
 set -e
 
 # If settings.php does not exist in config volume, copy default
-if [ ! -f /config/nfsen/settings.php ]; then
-    echo "No settings.php found in /config/nfsen. Copying default..."
-    cp /var/www/html/nfsen-ng/backend/settings/settings.php.dist /config/nfsen/settings.php
+if [ ! -f /config/nfsen-ng/settings.php ]; then
+    echo "No settings.php found in /config/nfsen-ng. Copying default..."
+    cp /var/www/html/nfsen-ng/backend/settings/settings.php.dist /config/nfsen-ng/settings.php
 fi
 
-# Link settings.php to NFSen-NG directory
-ln -sf /config/nfsen/settings.php /var/www/html/nfsen-ng/backend/settings/settings.php
+# Link settings.php to nfsen-ng directory
+ln -sf /config/nfsen-ng/settings.php /var/www/html/nfsen-ng/backend/settings/settings.php
 
 # Ensure correct ownership
 chown -R www-data:www-data /var/www/html
-chown -R www-data:www-data /data/nfsen
-chown -R www-data:www-data /config/nfsen
+chown -R www-data:www-data /data/nfsen-ng
+chown -R www-data:www-data /config/nfsen-ng
 
 # Execute container command
 exec "$@"
